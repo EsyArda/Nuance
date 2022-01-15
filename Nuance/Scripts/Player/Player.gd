22d7404a3374a980307857735a7afee2aefb49fd
HolyBlue <hugobarrandon@gmail.com>
2022-01-15 11:50:47 +0100

    Changement couleur joueur

--- Nuance/Scripts/Player/Player.gd
+++ Nuance/Scripts/Player/Player.gd
@@ -7,3 +7,7 @@ var velocity : Vector2 = Vector2()
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+
+func _on_Cursor_color_pick(color : Color):
+	$PlayerColor.color = color
