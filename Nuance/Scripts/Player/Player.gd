45b1bd430579f1fc20655910b1b76b0809655dba
HolyBlue <hugobarrandon@gmail.com>
2022-01-15 12:03:46 +0100

    move right left

--- Nuance/Scripts/Player/Player.gd
+++ Nuance/Scripts/Player/Player.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
 export (int) var gravity : int = 200
+export (int) var speed : int =100
 
 var velocity : Vector2 = Vector2()
 
 func _physics_process(delta):
+	velocity.x = get_horizontal_input() * speed
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	velocity = move_and_slide(velocity)
 
 
 func _on_Cursor_color_pick(color : Color):
 	$PlayerColor.color = color
+
+func get_horizontal_input():
+	var dirx = Input.get_action_strength("RIGHT") - Input.get_action_strength("LEFT")
+	return dirx
