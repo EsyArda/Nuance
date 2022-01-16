9c241f9c2403a354582f41630c1ce662fb54d754
HolyBlue <hugobarrandon@gmail.com>
2022-01-16 10:45:04 +0100

    Correction superposition niveau et skip de niveaux

--- Nuance/Scripts/Level/LevelTemplate.gd
+++ Nuance/Scripts/Level/LevelTemplate.gd
@@ -6,9 +6,12 @@ signal finish
 
 onready var spawn_point : Vector2 = $SpawnPoint.position
 
+export (bool) var reset_when_died : bool = false
+
 func _ready():
 	get_parent().set_spawn_point(spawn_point)
 
 func _on_AreaEnd_body_entered(body):
 	if body is Player:
-		emit_signal("finish")
+		if $AreaEnd.position.distance_to(body.position)<50:
+			emit_signal("finish")
